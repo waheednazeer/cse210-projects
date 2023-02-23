a91926b41b92c3bd813dcbf9ad1797282a022c0a
waheednazeer <waheed.nazeer@hotmail.com>
2023-02-23 08:59:31 +0500

    b

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,10 +4,10 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("What is first Name: ");
+        Console.Write("What is first Name? ");
         string fname=Console.ReadLine();
-        Console.WriteLine("What is your last Name? ");
+        Console.Write("What is your last Name? ");
         string lname=Console.ReadLine();
-        Console.WriteLine("Your Name is "+ lname +" ,"+fname+".");
+        Console.WriteLine($"Your Name is  {lname}, {fname} {lname}.");
     }
 }
