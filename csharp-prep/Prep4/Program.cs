82b9351beabe229eb2b7db5fe93202067c0c11f3
waheednazeer <waheed.nazeer@hotmail.com>
2023-02-23 13:34:20 +0500

    Prep4

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,43 @@
 using System;
 
+
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+
+        List <int> numbers= new List<int> ();
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+        int value=1;
+        string enterValue;
+        double sum=0;
+        int largest=0;
+        
+        while (value > 0)
+        {      
+            
+            Console.Write("Enter number: ");
+            enterValue= Console.ReadLine();
+            value=int.Parse(enterValue);
+            if (value > 0)
+            {
+               numbers.Add(value);  
+            }
+           
+        }
+        for (int i = 0; i < numbers.Count; i++)
+        {
+            sum=sum+numbers[i];
+            if (largest < numbers[i])
+            {
+                largest=numbers[i];
+            }
+            
+        }
+        double average= sum / numbers.Count;
+        Console.WriteLine($"Sum is {sum}");
+        Console.WriteLine($"The average is: {average}");
+        Console.WriteLine($"The largest is: {largest}");
+        
     }
 }
