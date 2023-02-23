51c922da99e3fd95ad7f54cc09134dd12d1bf7b0
waheednazeer <waheed.nazeer@hotmail.com>
2023-02-23 10:11:16 +0500

    Prep2 done

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,10 +4,11 @@ class Program
 {
     static void Main(string[] args)
     {
+        
         Console.Write("What is first Name? ");
-        string fname=Console.ReadLine();
+        string first=Console.ReadLine();
         Console.Write("What is your last Name? ");
-        string lname=Console.ReadLine();
-        Console.WriteLine($"Your Name is  {lname}, {fname} {lname}.");
+        string last=Console.ReadLine();
+        Console.WriteLine($"Your Name is  {last}, {first} {last}.");
     }
 }
--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,57 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("Enter your Percentage: ");
+        string userValue= Console.ReadLine();
+        int x= int.Parse(userValue);
+        string letter;
+
+        if (x >= 90)
+        {
+            if ((x % 10) > 3 )
+            {
+            letter="A";
+            }
+            else
+            {
+              letter="A-";   
+            }
+        }
+        else if (x >= 80)
+        {
+           if ((x % 10) > 3 )
+            {
+            letter="B+";
+            }
+            else
+            {
+              letter="B-";   
+            }
+        }
+        else if (x > 70)
+        {
+            if ((x % 10) > 3 )
+            {
+            letter="C+";
+            }
+            else
+            {
+              letter="C-";   
+            }
+        }
+       
+         else
+        {
+            letter="F";
+        }
+
+        if (x >70)
+        {
+            Console.WriteLine($"Congratulations you passed and got {letter}");
+        }
+        else
+        {
+          Console.WriteLine($"Next time work hard... {letter}");  
+        }
     }
 }
