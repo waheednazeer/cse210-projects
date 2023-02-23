d6abd36d77321933d5b9d4319b3696dffd1a2a8d
waheednazeer <waheed.nazeer@hotmail.com>
2023-02-23 11:50:42 +0500

    prep3 completed with stretch

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,38 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int majic = randomGenerator.Next(1, 101);
+
+        string response= "yes";
+        string guess;
+        int value;
+        int count=0;
+        while (response == "yes")
+        {
+            count++;
+          Console.Write("Guess the majic number ");
+            guess= Console.ReadLine();
+            value=int.Parse(guess);
+            if (value > majic)
+            {
+                Console.WriteLine("Majic number is Lower");
+            }
+            else if (value < majic)
+            {
+                Console.WriteLine("Majic number is Higher");
+            }
+            else
+            {
+                Console.WriteLine($"You guess it after {count} guesses.");
+                Console.Write("Do you want to play again? ");
+                response= Console.ReadLine();
+                if (response== "yes")
+                {
+                    majic = randomGenerator.Next(1, 101);
+                    count=0;
+                }
+            }
+        }
     }
 }
