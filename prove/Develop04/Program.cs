195196a233ec38f93cfb5b7782df00abfc3be50e
waheednazeer <waheed.nazeer@hotmail.com>
2023-03-18 12:43:50 +0500

    Activity logs as stretch added.

--- prove/Develop04/Program.cs
+++ prove/Develop04/Program.cs
@@ -7,6 +7,11 @@ class Program
         BreathingActivity bA = new BreathingActivity ("Breathing Activity", "This activity help you relax by walking you through breathing in and out slowly, \nclear your mind and focus on your breathing.");
         ReflectingActivity rA = new ReflectingActivity ("Reflecting Activity", "This actvity will help you reflect on times in your life when you have shown strength and resilience. \nThis will help you recognize the power you have and how you can use it in other aspects of your life.");
         ListingActivity lA = new ListingActivity("Listing Activity", "This activity will help you reflect on the good things in your life by having you list as many things as you can \nin a certain area.");
+        
+        int trackB = 0;
+        int trackR = 0;
+        int trackL = 0;
+
         string control="";
         while (control != "4")
         {
@@ -21,6 +26,7 @@ class Program
             control = Console.ReadLine();
             if (control == "1")
             {
+                trackB++;
                 Console.Clear();
                 Console.WriteLine(bA.GetActivityName());
                 Console.WriteLine(bA.GetActivityDescription());
@@ -31,14 +37,13 @@ class Program
                 Console.WriteLine("Get ready... ");
                 bA.RunSpinner();
                 bA.RunBreathingActivity(bA.GetActivityDuration());
-            
                 bA.DisplayEndingActivityMessage();    
             }
             
             if (control == "2")
             {
+                trackR++;
                 Console.Clear();
-                
                 Console.WriteLine(rA.GetActivityName());
                 Console.WriteLine(rA.GetActivityDescription());
                 Console.WriteLine();
@@ -48,11 +53,11 @@ class Program
                 Console.WriteLine("Get ready... ");
                 rA.RunSpinner();
                 rA.RunReflectionActivity(rA.GetActivityDuration());
-
                 rA.DisplayEndingActivityMessage();
             }
             if (control == "3")
             {
+                trackL++;
                 Console.Clear();
                 Console.WriteLine(lA.GetActivityName());
                 Console.WriteLine(lA.GetActivityDescription());
@@ -72,7 +77,14 @@ class Program
             }
 
         }
-
+        Console.Clear();
+        Console.WriteLine("Actvities log...");
+        rA.RunSpinner();
+        Console.WriteLine($"You repeated Breathing Activity {trackB} times in this session.");
+        rA.RunSpinner();
+        Console.WriteLine($"You repeated Reflecting Activity {trackR} times in this session.");
+        rA.RunSpinner();
+        Console.WriteLine($"You repeated Listing Activity {trackL} times in this session.");
         
     }
 }
